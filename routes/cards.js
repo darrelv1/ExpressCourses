42bc00a6f8ec68ed45932cefec2bed43f95290ae
Darrel Valdivieso <valdivieso4@hotmail.com>
2022-11-05 20:49:18 -0400

    Parameters, Query Strings, and Modularizing Routes

--- routes/cards.js
+++ routes/cards.js
@@ -3,15 +3,39 @@ const router = express.Router();
 const { data } = require("../data/flashCardData.json")
 const { cards } = data
 
+
 //Option one to had variables
 router.get('/:id',(request, response) => {
     const { id } = request.params
     const { side } = request.query
     const { hint } = cards[id];
+    let optiontext = "question"
     const text = cards[id][side]
+    const name = request.cookies.username
+    const templateData = {text,optiontext,name }
 
-    const templateData = {text , hint }
+    if (side === "question" ) {
+        templateData.hint = hint
+        optiontext = "answer";
+        templateData.optiontext = optiontext
+    } else if (!side) {
+        optiontext = "question"
+        templateData.optiontext = optiontext
+        response.redirect(`http://localhost:3001/cards/${id}?side=${optiontext}`)
+    }
+
+    let hyperlink = `http://localhost:3001/cards/${id}?side=${optiontext}`
+    templateData.hyperlink = hyperlink
+
+    console.log(hyperlink)
     response.render('cards', templateData);
 })
+
+router.get('/', (req, res)=> {
+    const rand = () =>  Math.floor(Math.random()* cards.length);
+    let redirectLink = `http://localhost:3001/cards/${rand()}`;
+    res.redirect(redirectLink)
+})
+
 
 module.exports = router
